18e772ae7be14c9e3c7a53015ea239c256dd3e0a
Izzywizz <isfandyar.ali@me.com>
2018-11-07 14:19:01 +0000

    added raycasting to allow point a shoot mechanics using mouse, fixed an issu with bobbly animation head

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     //Properties
+    public Rigidbody head;
+    public LayerMask layerMask; //what layer the ray (for raycasting) should hit
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
-    public Rigidbody head;
+    private Vector3 currentLookTarget = Vector3.zero; //where the marine should look
 
 	// Use this for initialization
 	void Start () {
@@ -28,16 +30,36 @@ public class PlayerController : MonoBehaviour {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //cal movement direction
 
         // Moving the marine head when the marine actually moves
-        if (moveDirection == Vector3.zero)
-        {
+        if (moveDirection == Vector3.zero) {
             // TODO
             //marine standing still
         }
-        else
-        {
+        else {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
             //you apply force in a paticular direction which is multipled by a force amount
+        }
 
+        RaycastHit hit; //empty hit but will be filled with a hit when it does
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //casting a ray from the camera to the mouse pointer
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);// This will draw agreen ray on to the screen   
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore)) {
+            //cast the ray with Pysics.Raycast
+            // pass in the ray that I've created along with the hit, out is just passing the reference of hit
+            // 1000 describe length of ray
+            // layermask lets the raycast is what we are trying to hit
+            // don't activate triggers
+
+            if (hit.point != currentLookTarget) {
+                currentLookTarget = hit.point; //impact point where the mouse is pointing thus update the marine where its pointing
+            }
+            //1 - obtain raycast/ target position, not the y coord becasue you want the marine to look fwd not at the floor
+            Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            // 2 - quaternion is rotation, to find the current position you subtract the target position from current then tell it where 
+            // to look using lookRotation
+            Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+            // 3 - actaully do the turn, Lerp allows the turing to be smooth over time by finding its location over time
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
         }
     }
 }
