c89bb16d098649c005e694d296fa668e7d99a3b4
Izzywizz <isfandyar.ali@me.com>
2018-11-07 10:47:49 +0000

    changed movement to use characterController rather then manually with ridgebody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    //Properties
     public float moveSpeed = 50.0f;
+    private CharacterController characterController;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        characterController = GetComponent<CharacterController>(); //init and get a refereence to current component CharacterController attached to the GameObject
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
+        //Create a new Vector3 and store the movement direction, then call the simpleMove method which takes the direction and speed and does the rest
+        // and also stops the character from going through walls/ obstecals
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
+             
+        /* Manual way of doing momvemnt with RidgeBody using ChracterController component
         Vector3 pos = transform.position; //current position of SpaceMarine in 3D Space (x, y, z)
 
         // retrives values from the input + 1(right) or - 1(left) based on key press, then multipiles this value with the moveSpeed
@@ -22,6 +30,7 @@ public class PlayerController : MonoBehaviour {
         pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
 
         transform.position = pos; //update marine position with the new position (pos)
+        */
 		
 	}
 }
