c8526fef67e19958363b5d6f661643698ee71d51
Izzywizz <isfandyar.ali@me.com>
2018-11-07 11:05:49 +0000

    removed surplus bullets prefab after being fire to save memory

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Methods
+
+    // when the bullet goes off camera, remove it this includes the scence view camera (All cameras!)
+    private void OnBeameVisible()
+    {
+        Destroy(gameObject);
+    }
+
+    //called during a collision, the collision object gives you info about the target collision as well.
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
